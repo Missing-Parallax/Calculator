a90549cc2289230c9e93df46390298e813df51d2
Missing-Parallax <104851278+Missing-Parallax@users.noreply.github.com>
2023-11-06 03:07:34 +0530

    Added JS and functionality

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 
 
 let screen=document.querySelector('input');
-
 let button=document.querySelectorAll('button');
 
-for(item in button){
-    item.addEven
+for(item of button){
+    item.addEventListener('click',(event)=>{
+        if(event.target.innerText=='C') screen.value="";
+        else if(event.target.innerText=='='){
+            try{
+                screen.value=eval(screen.value);
+            }
+            catch(err){
+                screen.value="Invalid";
+            }
+        }
+        else{
+            screen.value+=(event.target.innerText);
+        }
+    })
 }
