972d6a602793df4a01f1a3b4f3b6572f7d0783cb
Missing-Parallax <104851278+Missing-Parallax@users.noreply.github.com>
2023-11-16 03:20:31 +0530

    Bug fixes
    Fixed some issues related to responsiveness of the code and fixed some bugs where user could type alphabets in the input field

--- app.js
+++ app.js
@@ -20,9 +20,11 @@ for(item of button){
     })
 }
 
-let calci= document.querySelector('html');
-calci.addEventListener('keydown',(event)=>{
-    console.log(event.key)
+screen.addEventListener('keydown',function(e){
+    e.preventDefault();
+})
+
+document.addEventListener('keydown',(event)=>{
     if(event.key=="Enter"){
         try{
             screen.value=eval(screen.value);
@@ -31,4 +33,14 @@ calci.addEventListener('keydown',(event)=>{
             screen.value="Invalid";
         }
     }
+    else if(event.key=="Backspace") screen.value=screen.value.substring(0,screen.value.length-1);
+    else screen.value+=isValid(event.key);
 })
+
+
+function isValid(e){
+    let arr=['1','2','3','4','5','6','7','8','9','0','*',"%",'/','-','+'];
+
+    for(item of arr) if(e==item) return item;
+    return "";
+}
